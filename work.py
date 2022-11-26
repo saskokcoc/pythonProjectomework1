1758dff096e6d55e8fe6bdc6b504d8aed82983a9
saskokcoc <koreshsashano@gmail.com>
2022-11-26 08:17:21 +0200

    padote

---
+++ work.py
@@ -0,0 +1,14 @@
+class Тварини(Живі):
+    def дихати(self):
+        pass
+
+    def рухатись(self):
+        pass
+
+    def харчуватись(self):
+        pass
+
+
+class Кошеня(Тварини):
+    def пити_молоко(self):
+        pas
